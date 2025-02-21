90637fdcb0668f9f24962b7ff9d168f870d3406a
kurtchristianmanla <kurtchristianmanla@gmail.com>
2025-02-22 02:48:17 +0800

    Remove required imageUrl

--- backend/models/order.js
+++ backend/models/order.js
@@ -37,7 +37,7 @@ const orderSchema = new mongoose.Schema({
         },
         imageUrl: {
             type: String,
-            required: true
+            // required: true
         }
     }],
   
